723bdef6c5d7f0d9b886327116bc8098be53c103
Gerald Q. Maguire Jr <maguire@kth.se>
2022-03-09 17:28:06 +0100

    Added support for collecting incorrect answers for short answer and fill in multiple blanks questions.

--- augment_quizzes-and-answers-in-course.py
+++ augment_quizzes-and-answers-in-course.py
@@ -173,7 +173,8 @@ def main():
 
     print("quiz_info={}".format(quiz_info))
 
-    incorrect_answers=dict()    # values student as a list under the question_id
+    incorrect_answers=dict()    # values a set under the question_id
+    incorrect_answers_multiple_blanks=dict() #  values are stored as quiest_id: {blank_id_hash: {value set}}
 
     quiz_submissions_all=dict()
     for quiz in quiz_info:
@@ -182,14 +183,20 @@ def main():
 
         # add an empty column to remeber the questions that were ask
         quiz_submissions_all[quiz]['questions_asked']=''
+        quiz_submissions_all[quiz]['questions_types']=''
+        quiz_submissions_all[quiz]['answer_correctness']=''
 
         for index, row in  quiz_submissions_all[quiz].iterrows():
             # make a directory for the submissions from result_url
             # the URLs have the form: https://canvas.kth.se/courses/11/quizzes/39141/history?quiz_submission_id=759552&version=1
             url=row['result_url']
             questions_asked=dict()
+            questions_types=dict()
+            answer_correctness=dict()
+            version_string=''
             dir_name=dir_name_for_urls(url, target_dir)
             for file in os.listdir(dir_name):
+                version_string=''
                 if file.endswith(".html"):
                     file_name=os.path.join(dir_name, file)
                     html_string=''
@@ -206,6 +213,9 @@ def main():
                         version_string=file_name[version_offset:-5]
                         print("version_string={}".format(version_string))
 
+                    if len(html_string) == 0:
+                        print("No contents for file={}".format(file_name))
+                        continue
                     document = html.document_fromstring(html_string)
                     display_questions = document.xpath('//*[contains(@class, "display_question")]')
                     # Can have a correct answer:
@@ -222,40 +232,112 @@ def main():
                     # 
                     # Remember the questions ask in this attempt
                     question_asked_this_attempt=list()
+                    questions_types_this_attempt=list()
+                    answer_correctness_this_attempt=list()
+
                     for a_question in display_questions:
+                        question_type=None
                         a_question_id=a_question.attrib['id']
                         a_question_class=a_question.attrib['class']
                         if Verbose_Flag:
                             print("question id={0} class={1}".format(a_question_id, a_question_class))
                         question_id=int(a_question_id[len(question_id_prefix):])
                         question_asked_this_attempt.append(question_id)
-                    questions_asked[version_string]=question_asked_this_attempt
+                        if 'partial_credit' in a_question_class:
+                            answer_correctness_this_attempt.append('partial_credit')
+                        elif 'incorrect' in a_question_class:
+                            answer_correctness_this_attempt.append('incorrect')
+                        else:
+                            # otherwise assume it is correct
+                            answer_correctness_this_attempt.append('correct')
 
-                    inputs = document.xpath('//*[@class="question_input"]')
-                    #<input type="text" name="question_274239" value="effective" class="question_input" autocomplete="off" readonly="&quot;readonly&quot;" aria-label="Fill in the blank answer" aria-describedby="user_answer_NaN_arrow" style="width: 85.5px;">
-                    for input in inputs:
-                        input_id=input.attrib.get('id', None)
-                        input_type=input.attrib.get('type', None)
-                        input_name=input.attrib.get('name', None)
-                        input_value=input.attrib.get('value', None)
-                        input_describedby=input.attrib.get('aria-describedby', None)
-                        if input_describedby == 'user_answer_NaN_arrow':
-                            question_id=int(input_name[len(question_id_prefix):])                            
-                            current_incorrect_answers=incorrect_answers.get(question_id, None)
-                            if current_incorrect_answers:
-                                incorrect_answers[question_id]=current_incorrect_answers.append(input_value)
-                            else:
-                                incorrect_answers[question_id]=[input_value]
-                        print("input_id={0}, input_type={1},input_name={2},input_value={3},input_describedby={4}".format(input_id, input_type, input_name, input_value, input_describedby))
+                        if 'multiple_answers_question' in a_question_class:
+                            question_type='multiple_answers_question'
+                        elif 'short_answer' in  a_question_class:
+                            question_type='short_answer'
+                        elif 'true_false_question' in  a_question_class:
+                            question_type='true_false_question'
+                        elif 'fill_in_multiple_blanks_question' in  a_question_class:
+                            question_type='fill_in_multiple_blanks_question'
+                        elif 'multiple_choice_question' in  a_question_class:
+                            question_type='multiple_choice_question'
+                        elif 'matching_question' in  a_question_class:
+                            question_type='matching_question'
+                        else:
+                            question_type=a_question_class
+
+                        questions_types_this_attempt.append(question_type)
+
+                        #  note that this is a relative query, hence the leading "." - so we only look at answers to this question
+                        inputs = a_question.xpath('.//*[@class="question_input"]')
+                        #<input type="text" name="question_274239" value="effective" class="question_input" autocomplete="off" readonly="&quot;readonly&quot;" aria-label="Fill in the blank answer" aria-describedby="user_answer_NaN_arrow" style="width: 85.5px;">
+                        # version_string
+                        for a_number, input in enumerate(inputs):
+                            print("a_number={}".format(a_number))
+                            input_id=input.attrib.get('id', None)
+                            input_type=input.attrib.get('type', None)
+                            input_name=input.attrib.get('name', None)
+                            input_value=input.attrib.get('value', None)
+                            input_describedby=input.attrib.get('aria-describedby', None)
+                            if not input_value: # skip case of no answer
+                                continue
+
+                            if 'incorrect' in a_question_class and input_type=='text':
+                                print("input_id={0}, input_type={1},input_name={2},input_value={3},input_describedby={4}, question_type={5}".format(input_id, input_type, input_name, input_value, input_describedby, question_type))
+                                split_input_name=input_name.split('_')
+                                question_id=int(split_input_name[1])
+                                if len(split_input_name) > 2:
+                                    print("extention to input_name for question_id={0} of {1}".format(question_id, split_input_name))
+                                if question_type == 'fill_in_multiple_blanks_question':
+                                    # note that quseions with multiple blanks have gensymn'd extensions to the name:
+                                    # input_name=question_274226_443f6a7201fd1b9c37119a9d1aa28776 - has the blank_id='a0'
+                                    # input_name=question_274226_d5157c76409909bc0db8eea11b8b37d8 - has the blank_id='a1'
+                                    # However, I do not know (yet) how to know that the above are the mappings (except by examinging the question manually)
+                                    current_incorrect_answers=incorrect_answers_multiple_blanks.get(question_id, None)
+                                    blank_id_hash=split_input_name[2]
+                                    if current_incorrect_answers:
+                                        incorrect_answers_for_blank=current_incorrect_answers.get(blank_id_hash, None)
+                                        if incorrect_answers_for_blank:
+                                            incorrect_answers_for_blank.add(input_value) #  note that the add occurs in place
+                                        else:
+                                            incorrect_answers_multiple_blanks[question_id][blank_id_hash]= {input_value}
+                                    else:
+                                        incorrect_answers_multiple_blanks[question_id]={blank_id_hash: {input_value}}
+                                else: #  not a multiple blank question
+                                    current_incorrect_answers=incorrect_answers.get(question_id, None)
+                                    if current_incorrect_answers:
+                                        current_incorrect_answers.add(input_value) #  note that the add occurs in place
+                                        print("first case: input_value={0}, incorrect_answers[{1}]={2}".format(input_value, question_id, incorrect_answers[question_id]))
+                                    else:
+                                        incorrect_answers[question_id]={input_value}
+                                        print("second case: input_value={0}, incorrect_answers[{1}]={2}".format(input_value, question_id, incorrect_answers[question_id]))
+
+
+
+                    questions_asked[version_string]=question_asked_this_attempt
+                    questions_types[version_string]=questions_types_this_attempt
+                    answer_correctness[version_string]=answer_correctness_this_attempt
+
 
             if questions_asked:
                 print("questions_asked={}".format(questions_asked))
                 quiz_submissions_all[quiz].at[index, 'questions_asked']=questions_asked
 
+            if questions_types:
+                print("questions_types={}".format(questions_types))
+                quiz_submissions_all[quiz].at[index, 'questions_types']=questions_types
+
+            if answer_correctness:
+                print("answer_correctness={}".format(answer_correctness))
+                quiz_submissions_all[quiz].at[index, 'answer_correctness']=answer_correctness
+
+
     # the following was inspired by the section "Using XlsxWriter with Pandas" on http://xlsxwriter.readthedocs.io/working_with_pandas.html
     # set up the output write
     writer = pd.ExcelWriter('quizzes-'+course_id+'-augmented.xlsx', engine='xlsxwriter')
     quizzes_df.to_excel(writer, sheet_name='Quizzes')
+
+    quiz_questions_to_quiz=dict() # for each question_id record what quiz it appears in
 
     for quiz in quiz_info:
         # copy over the quiz data
@@ -263,6 +345,12 @@ def main():
         print("copying quiz={}".format(sheet_name))
         quiz_instance = pd.read_excel(open(input_file, 'rb'), sheet_name=sheet_name)
         quiz_instance.to_excel(writer, sheet_name=sheet_name)
+
+        for index, row in  quiz_instance.iterrows():        
+            if quiz_questions_to_quiz.get(row['id'], None):
+                print("question_id={0} already has a quiz_id {1} associated with it".format(row['id'], row['quiz_id']))
+            else:
+                quiz_questions_to_quiz[row['id']]=row['quiz_id']
 
         # save the aumented quiz submission information
         sheet_name="s_{}".format(quiz)
@@ -273,7 +361,16 @@ def main():
     writer.save()
 
     for q in incorrect_answers:
-        print("Incorrect answers for {0}: {1}".format(q, incorrect_answers[q]))
+        try:
+            print("Incorrect answers for {0} on quiz {1}: {2}".format(q, quiz_questions_to_quiz[q], incorrect_answers[q]))
+        except:
+            print("Invalid question id {0} (not in the list of questions - but has incorrect answers={1}".format(q, incorrect_answers[q]))
+
+    for q in incorrect_answers_multiple_blanks:
+        try:
+            print("Incorrect answers for {0} on quiz {1}: {2}".format(q, quiz_questions_to_quiz[q], incorrect_answers_multiple_blanks[q]))
+        except:
+            print("Invalid question id {0} (not in the list of questions - but has incorrect answers={1}".format(q, incorrect_answers_multiple_blanks[q]))
 
 
 if __name__ == "__main__": main()
