b7ad7f2089067ad720c3b551f87568d1f887ae7d
Gerald Q. Maguire Jr <maguire@kth.se>
2024-11-05 12:44:36 +0100

    More corrected abstracts.

--- miss_spelled_to_correct_spelling_SCI.py
+++ miss_spelled_to_correct_spelling_SCI.py
@@ -1597,7 +1597,7 @@ miss_spelled_to_correct_spelling={
     ' bea ': {'c': ' be a ', 's': 'diva2:1528127'},
     'Inan ': {'c': 'In an ', 's': 'diva2:1741184'},
     ' ina ': {'c': ' in a ', 's': ['diva2:1737438', 'diva2:488441']},
-    ' ata ': {'c': ' a ta ', 's': ['diva2:1780558', 'diva2:1770544']},
+    ' ata ': {'c': '  at a ', 's': ['diva2:1780558', 'diva2:1770544']},
     ' interestin ': {'c': ' interest in ', 's': 'diva2:783984'},
     ' isto ': {'c': ' is to ', 's': ['diva2:1142926', 'diva2:1142952', 'diva2:1120353', 'diva2:1509441', 'diva2:1781251', 'diva2:1120590', 'diva2:1519872']},
     ' ona ': {'c': ' on a ', 's': ['diva2:1285492', 'diva2:1698044']},
