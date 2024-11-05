8ee979458049397ca3741f1bcfcff30b0bfc46ca
Gerald Q. Maguire Jr <maguire@kth.se>
2024-11-05 03:42:06 +0100

    More corrected abstracts.

--- miss_spelled_to_correct_spelling_SCI.py
+++ miss_spelled_to_correct_spelling_SCI.py
@@ -1602,6 +1602,8 @@ miss_spelled_to_correct_spelling={
     ' isto ': {'c': ' is to ', 's': ['diva2:1142926', 'diva2:1142952', 'diva2:1120353', 'diva2:1509441', 'diva2:1781251', 'diva2:1120590', 'diva2:1519872']},
     ' ona ': {'c': ' on a ', 's': ['diva2:1285492', 'diva2:1698044']},
     ' isin ': {'c':' is in ', 's': 'diva2:1464101'},
+    'Rigde': {'c': 'Ridge', 's': 'diva2:1678918', 'n': 'error in original'},
+    " it's ": {'c': " its ", 's': 'diva2:1678918', 'n': 'error in original'},
 }
 
 junklist=[
