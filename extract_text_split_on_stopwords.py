d2392127a7c989e3d352e7f931b3841719bace13
Gerald Q. Maguire Jr <maguire@kth.se>
2026-02-14 18:39:35 +0100

    Added more acronyms and terms.

--- extract_text_split_on_stopwords.py
+++ extract_text_split_on_stopwords.py
@@ -1208,7 +1208,7 @@ def extract_text_from_pdf(pdf_path):
                     continue
                 
                 # special case for thesis with missnumbered page 1
-                if options.Qcase and pageno < 24:
+                if options.Qcase and pageno < 16:
                     continue
                 
 
